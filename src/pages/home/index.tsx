b846dfb4e85f08c2b08c2ac6149dc7e534291aab
wliu <wliu@relaper.com>
2023-10-09 21:57:06 +0800

    modify: 可以用了

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from 'react';
 import Style from './index.less';
-import { request } from '@/request';
+import { request } from '@flc/workbench-tools';
 
 export default function home() {
 
   useEffect(() => {
-    request('/proxy/user/logout', {
+    request('/user/logout', {
       method: 'get',
     }).then(res => {
-      console.log(res)
-    })
-  }, [])
+      console.log(res);
+    });
+  }, []);
 
   return (
     <main className={Style['home']}>home</main>
